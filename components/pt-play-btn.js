ded2f72b61297b5e25d60615997f5b5bc6814793
Oyunbat11 <yuka81683@gmail.com>
2025-04-04 22:50:29 +0800

    home responsive update

--- components/pt-play-btn.js
+++ components/pt-play-btn.js
@@ -5,7 +5,6 @@ class PlayBtn extends HTMLElement {
 
     connectedCallback() {
         
-        // Create button
         const btn = document.createElement("button");
         btn.innerHTML = `<i class="fas fa-play"></i> ТОГЛУУЛАХ`;
         btn.className = "btn-play"; 
@@ -32,4 +31,45 @@ class AddBtn extends HTMLElement {
 }
 
 customElements.define("pt-add-btn", AddBtn);
+
+class HeaderMenu extends HTMLElement {
+
+    constructor(){
+        super();
+    }
+
+    connectedCallback(){
+
+        const header = document.createElement("div");
+        header.innerHTML = 
+        `<div class="header-left-container">
+                <div class="logo"><a href="#">PlayTV</a></div>
+                <nav>
+                    <ul>
+                        <li><a href = "#">Нүүр</a></li>
+                        <li><a href = "#">TV</a></li>
+                        <li><a href = "#">BOX</a></li>
+                        <li><a href = "#">Миний жагсаалт</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="header-right-container">
+                <div class="user-actions">
+                    <button><i class="fas fa-search"></i></button>
+                    <button><i class="fas fa-bell"></i></button>
+                    <button><i class="fas fa-user"></i></button>
+                </div>
+            </div>`;
+
+            header.className = "main-header-container";
+
+            this.appendChild(header);
+
+    }
+
+}
+
+customElements.define("pt-header-menu",HeaderMenu);
+
+
 
