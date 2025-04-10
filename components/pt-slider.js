d9c3579bf8324668391c19207b728d87c6e02782
Oyunbat11 <yuka81683@gmail.com>
2025-04-10 23:44:15 +0800

    Component udpate

--- components/pt-slider.js
+++ components/pt-slider.js
@@ -1,3 +1,60 @@
+const sliderData = [
+    {
+        title: "Didi",
+        category: "Инээдмийн",
+        duration: 110,
+        poster: "posters/DidiHBO_vertical.jpg"
+    },
+    {
+        title: "Амьдрах Сайхан",
+        category: "Драм",
+        duration: 120,
+        poster: "posters/AmidrahSaihanS01_vertical.jpg"
+    },
+    {
+        title: "Махан Бөмбөг",
+        category: "Анимейшн",
+        duration: 90,
+        poster: "posters/CloudyWithAChanceOfMeatballs_vertical.jpg"
+    },
+    {
+        title: "DespicableMe 4",
+        category: "Анимейшн",
+        duration: 100,
+        poster: "posters/DespicableMe4HBO_vertical.jpg"
+    },
+    {
+        title: "KungFu Panda 4",
+        category: "Анимейшн",
+        duration: 115,
+        poster: "posters/KungfuPanda4HBO_vertical.jpg"
+    },
+    {
+        title: "Өв Залгамжлагч",
+        category: "Инээдмийн",
+        duration: 120,
+        poster: "posters/OvZalgamchlagchS01_verticalnotag.jpg"
+    },
+    {
+        title: "Ratatouille",
+        category: "Анимейшн",
+        duration: 130,
+        poster: "posters/Ratatouille_vertical.jpg"
+    },
+    {
+        title: "Skyscraper",
+        category: "Адал явдалт",
+        duration: 105,
+        poster: "posters/Skyscraper_vertical.jpg"
+    },
+    {
+        title: "Zura",
+        category: "Адал явдалт",
+        duration: 125,
+        poster: "posters/ztsuram_vertical.jpg"
+    },
+];
+
 let visibleCards = 6;
 const scrollStep = 3;
 
@@ -11,8 +68,6 @@ document.addEventListener('DOMContentLoaded',() => {
     if (cardWidth < 220){
         visibleCards = 3;
     } else{ }
-
-    console.log(visibleCards);
 
     sections.forEach(section => {
 
@@ -37,7 +92,7 @@ document.addEventListener('DOMContentLoaded',() => {
         position = Math.max(position - scrollStep, 0);
         updateSliderpos();
       });
-
+      
     });
 
 });
@@ -47,8 +102,31 @@ class MovieCard extends HTMLElement {
         super();
     }
 
+    render() {
 
+        this.innerHTML = 
+        `
+            <div class="movie-card">
+                <div class="card-content-container">
+                    <button class="btn-add-card"><i class="fas fa-plus"></i></button>
+                    <div class="card-content">
+                        <h4 class="card-title">Movie Name</h4>
+                        <span class="card-description">
+                            <h6>Төрөл</h6>
+                            <h6>120 мин</h6>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        `
+    }
+
+    connectedCallback(){
+        this.render();
+    }
 }
+
+customElements.define("pt-movie-card", MovieCard);
 
 class MovieSlider extends HTMLElement {
 
@@ -65,15 +143,15 @@ class MovieSlider extends HTMLElement {
               <div class="slider-row-wrapper">
               <button class="slider-arrow prev-arrow" id="prevBtn"><</button>
                   <div class="movie-list-container">
-                      <div class="movie-card"></div>
-                      <div class="movie-card"></div>
-                      <div class="movie-card"></div>
-                      <div class="movie-card"></div>
-                      <div class="movie-card"></div>
-                      <div class="movie-card"></div>
-                      <div class="movie-card"></div>
-                      <div class="movie-card"></div>
-                      <div class="movie-card"></div>
+                      <pt-movie-card></pt-movie-card>
+                      <pt-movie-card></pt-movie-card>
+                      <pt-movie-card></pt-movie-card>
+                      <pt-movie-card></pt-movie-card>
+                      <pt-movie-card></pt-movie-card>
+                      <pt-movie-card></pt-movie-card>
+                      <pt-movie-card></pt-movie-card>
+                      <pt-movie-card></pt-movie-card>
+                      <pt-movie-card></pt-movie-card>
                   </div>
               <button class="slider-arrow next-arrow" id="nextBtn">></button>
               </div>
